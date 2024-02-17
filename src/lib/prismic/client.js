caf7be9e2f95ca093de756891f02c10d4a05a5e3
Karly Hoffman <karlyhoffman@gmail.com>
2024-02-17 15:50:18 -0600

    prismic config — update routes

--- src/lib/prismic/client.js
+++ src/lib/prismic/client.js
@@ -2,21 +2,20 @@ import * as prismic from "@prismicio/client";
 
 const { PRISMIC_API_URL, PRISMIC_ACCESS_TOKEN: accessToken } = import.meta.env;
 
-// const routes = [
-//   {
-//     type: "recipe",
-//     path: "/recipes/:uid",
-//   },
-//   {
-//     type: "ingredient_tag",
-//     path: "/recipes/ingredients/:uid",
-//   },
-//   { type: "cuisine_tag", path: "/recipes/cuisines/:uid" },
-//   { type: "type_tag", path: "/recipes/dish-types/:uid" },
-//   { type: "season_tag", path: "/recipes/seasons/:uid" },
-// ];
-
-const routes = [];
+const routes = [
+  {
+    type: "homepage",
+    path: "/",
+  },
+  {
+    type: "plane",
+    path: "/planes/:uid",
+  },
+  {
+    type: "custom_template",
+    path: "/:uid",
+  },
+];
 
 export const client = prismic.createClient(PRISMIC_API_URL, {
   accessToken,

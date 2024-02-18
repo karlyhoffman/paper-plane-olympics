28820f071cf46bd85d3ec9105dce5a2af6a20dbd
Karly Hoffman <karlyhoffman@gmail.com>
2024-02-17 20:13:32 -0600

    custom template — initial template build

--- src/lib/prismic/queries.js
+++ src/lib/prismic/queries.js
@@ -3,5 +3,8 @@ import { client } from "./client.js";
 export const fetchSingleDocumentByType = async ({ type = "", options = {} }) =>
   client.getSingle(type, options);
 
+// export const fetchSingleDocumentByUID = async ({ uid = "", options = {} }) =>
+//   client.getByUID(uid, options);
+
 export const fetchDocumentsByType = async ({ type = "", options = {} }) =>
   client.getByType(type, options).then((res) => res.results);

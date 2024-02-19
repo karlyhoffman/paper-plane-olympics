f7620237217b054dc02d14a56a07748e2cd879b4
Karly Hoffman <karlyhoffman@gmail.com>
2024-02-19 17:26:04 -0600

    components — refactor; create index file

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Footer } from "./Footer.astro";
+export { default as Planes } from "./Planes.astro";
+export { default as PrismicHtml } from "./PrismicHtml.astro";
+export { default as SEO } from "./SEO.astro";
+export { default as TitleCard } from "./TitleCard.astro";

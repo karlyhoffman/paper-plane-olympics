6b3d2833e1bc62f082022bffb67954bf6cd5b259
Karly Hoffman <karlyhoffman@gmail.com>
2024-02-19 21:35:48 -0600

    card component — initial build and style

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
-export { default as Footer } from "./Footer.astro";
-export { default as Planes } from "./Planes.astro";
-export { default as PrismicHtml } from "./PrismicHtml.astro";
-export { default as SEO } from "./SEO.astro";
-export { default as TitleCard } from "./TitleCard.astro";
+export { default as Card } from './Card.astro';
+export { default as Footer } from './Footer.astro';
+export { default as Planes } from './Planes.astro';
+export { default as PrismicHtml } from './PrismicHtml.astro';
+export { default as SEO } from './SEO.astro';
+export { default as TitleCard } from './TitleCard.astro';

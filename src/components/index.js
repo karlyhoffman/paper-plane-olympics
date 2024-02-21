fe90f5917ae3abbcaceeb93e8ef13462daa4e8f4
Karly Hoffman <karlyhoffman@gmail.com>
2024-02-20 22:37:14 -0600

    components — add conditional wrapper

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 export { default as Card } from './Card.astro';
 export { default as Footer } from './Footer.astro';
+export { default as ConditionalWrapper } from './ConditionalWrapper.astro';
 export { default as Planes } from './Planes.astro';
 export { default as PrismicHtml } from './PrismicHtml.astro';
 export { default as SEO } from './SEO.astro';

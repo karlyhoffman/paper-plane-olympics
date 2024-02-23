d9c0cbe792eec501ca0355dd01b62ba61c71de24
Karly Hoffman <karlyhoffman@gmail.com>
2024-02-22 18:13:38 -0600

    add vercel plugins

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
-import { defineConfig } from 'astro/config';
+import vercel from "@astrojs/vercel/serverless";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "hybrid",
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
+});

aa598938e20d94b462137be491e61de8823f5f8d
Karly Hoffman <karlyhoffman@gmail.com>
2024-02-22 18:14:20 -0600

    global styles — minor tweaks

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,8 @@
-export { default as Card } from './Card.astro';
-export { default as Footer } from './Footer.astro';
-export { default as ConditionalWrapper } from './ConditionalWrapper.astro';
-export { default as Planes } from './Planes.astro';
-export { default as PrismicHtml } from './PrismicHtml.astro';
-export { default as SEO } from './SEO.astro';
-export { default as TitleCard } from './TitleCard.astro';
+export { default as Card } from "./Card.astro";
+export { default as Footer } from "./Footer.astro";
+export { default as ConditionalWrapper } from "./ConditionalWrapper.astro";
+export { default as Planes } from "./Planes.astro";
+export { default as PrismicHtml } from "./PrismicHtml.astro";
+export { default as SEO } from "./SEO.astro";
+export { default as SliceZone } from "./SliceZone.astro";
+export { default as TitleCard } from "./TitleCard.astro";

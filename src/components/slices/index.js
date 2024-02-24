1075a86202013f5766599f65e89bf4b10de5e841
Karly Hoffman <karlyhoffman@gmail.com>
2024-02-24 12:05:40 -0600

    slice zone — add initial planes index component

--- src/components/slices/index.js
+++ src/components/slices/index.js
@@ -2,5 +2,6 @@ export { default as CTA } from "./CTA.astro";
 export { default as HeroImage } from "./HeroImage.astro";
 export { default as HeroSimple } from "./HeroSimple.astro";
 export { default as Image } from "./Image.astro";
+export { default as PlanesIndex } from "./PlanesIndex.astro";
 export { default as Text } from './Text.astro';
 export { default as TextWithImage } from './TextWithImage.astro';

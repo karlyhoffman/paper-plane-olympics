0a3748e0e6612d86994b649b0ba69a5ef58c440b
Karly Hoffman <karlyhoffman@gmail.com>
2024-02-24 11:34:52 -0600

    slice zone — add initial image component

--- src/components/slices/index.js
+++ src/components/slices/index.js
@@ -1,4 +1,5 @@
 export { default as HeroImage } from "./HeroImage.astro";
 export { default as HeroSimple } from "./HeroSimple.astro";
+export { default as Image } from "./Image.astro";
 export { default as Text } from './Text.astro';
 export { default as TextWithImage } from './TextWithImage.astro';

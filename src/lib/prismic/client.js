c5e6022bd7e714c5d878a171025d6a739687cd89
Karly Hoffman <karlyhoffman@gmail.com>
2024-02-23 19:26:26 -0600

    minor cleanup

--- src/lib/prismic/client.js
+++ src/lib/prismic/client.js
@@ -3,13 +3,15 @@ import * as prismic from "@prismicio/client";
 const { PRISMIC_API_URL, PRISMIC_ACCESS_TOKEN: accessToken } = import.meta.env;
 
 /*
- * Configuring the route array below is optional, however, when routes are define 
+ * Configuring the route array below is optional, however, when routes are define
  * Prismic will return the generated url in the fetch response.
- * 
+ *
  * For example, if you have a document with the type "plane" and the uid "plane-jane",
  * the fetch response will include the url field with the value "/planes/plane-jane".
- * 
+ *
  * If you don't define routes, the url field will not be included in the fetch response.
+ *
+ * https://prismic.io/docs/route-resolver#example
  */
 const routes = [
   {

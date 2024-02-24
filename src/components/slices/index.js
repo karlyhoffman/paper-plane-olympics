779e855aea7b2b9597ee9d2b97030ed5edf9588c
Karly Hoffman <karlyhoffman@gmail.com>
2024-02-24 11:43:15 -0600

    slice zone — add initial CTA component

--- src/components/slices/index.js
+++ src/components/slices/index.js
@@ -1,3 +1,4 @@
+export { default as CTA } from "./CTA.astro";
 export { default as HeroImage } from "./HeroImage.astro";
 export { default as HeroSimple } from "./HeroSimple.astro";
 export { default as Image } from "./Image.astro";

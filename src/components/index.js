21540b9643d5ed0a360307f5f5b745773a44f4b6
Karly Hoffman <karlyhoffman@gmail.com>
2024-03-03 15:06:38 -0600

    plane animation — file cleanup; breakup plane files individually for intersection observer

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,7 @@ export { default as Card } from "./Card.astro";
 export { default as ConditionalWrapper } from "./ConditionalWrapper.astro";
 export { default as Footer } from './Footer.astro';
 export { default as Navbar } from './Navbar.astro';
-export { default as Planes } from "./Planes.astro";
+export { default as Plane } from "./Plane.astro";
 export { default as PrismicHtml } from "./PrismicHtml.astro";
 export { default as SEO } from "./SEO.astro";
 export { default as SliceZone } from "./SliceZone.astro";
